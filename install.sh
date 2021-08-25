dd0e7a1efa23e6449a6e536f2d8f82b389fae017
oliverdding <oliverdding@outlook.com>
2021-08-25 16:42:36 +0800

    update

--- install.sh
+++ install.sh
@@ -88,3 +88,7 @@ echo "############################"
 echo "installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+touch ~/.config/git/git-credentials
+chmod 600 ~/.config/git/git-credentials
