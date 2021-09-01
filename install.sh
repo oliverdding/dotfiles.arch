56fbf2d0e668d56a5e5bffc4a9700030fe904eef
oliverdding <oliverdding@outlook.com>
2021-09-01 18:41:57 +0800

    modify PATH

--- install.sh
+++ install.sh
@@ -93,7 +93,6 @@ echo "############################"
 echo "installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 vim +PlugInstall +qall
 
 touch ~/.config/git/git-credentials
