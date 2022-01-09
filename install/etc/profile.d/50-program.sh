d77252718c3f606c3350f8acec94fb3ec8795dde
oliverdding <oliverdding@outlook.com>
2022-01-10 00:10:33 +0800

    update sbt opts

--- install/etc/profile.d/50-program.sh
+++ install/etc/profile.d/50-program.sh
@@ -1,3 +1,4 @@
 export DOCKER_BUILDKIT="1"
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
 export GOPROXY="https://goproxy.io,direct"
+export SBT_OPTS="-Dsbt.override.build.repos=true"
